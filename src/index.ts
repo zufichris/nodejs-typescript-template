c8431efad74fdd580620ea74f77b9c12ad40453b
zufichris <zufichris@gmail.com>
2024-10-10 23:06:04 +0400

    setup express app

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,34 @@
 import express from "express";
 import { Start } from "./server/httpServer";
-import { errorHandler } from "./middleware/error/errorMiddleware";
+import { errorHandler, AppError } from "./middleware/error/errorMiddleware";
 import Router from "./services/routes/route_v1";
 import notFoundMiddleware from "./middleware/error/notfound";
 const app = express();
-
+app.use(
+  express.json({
+    limit: 100,
+    verify(req, res, buf, encoding) {
+      if (buf.byteLength > 100) {
+        throw new AppError({
+          message: "File size too large",
+        });
+      }
+    },
+  })
+);
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: 100,
+    verify(req, res, buf, encoding) {
+      if (buf.byteLength > 100) {
+        throw new AppError({
+          message: "File size too large",
+        });
+      }
+    },
+  })
+);
 app.use(Router);
 app.use(notFoundMiddleware);
 app.use(errorHandler);
