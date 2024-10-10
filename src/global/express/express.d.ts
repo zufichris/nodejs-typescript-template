d5af3a8a2aa04a2e3b70a768aff9624c7803b20e
zufichris <zufichris@gmail.com>
2024-10-10 04:25:08 +0400

    globals

---
+++ src/global/express/express.d.ts
@@ -0,0 +1,10 @@
+//Adding Custom Properties in Express Namespace
+
+import * as express from "express"; //This makes sure you're extending  existing interfaces rather than defining a new ones
+
+declare namespace Express {
+  interface Request {
+    userId: string;
+  }
+  //Add More Properties
+}
